c7ff659137f1fc02b421a2ebfb426ff7f94d906e
“SctoC” <hanxiangqian2021@163.com>
2024-08-01 11:41:46 +0800

    意外

--- good_job_client/LogIn_dlg.h
+++ good_job_client/LogIn_dlg.h
@@ -11,8 +11,8 @@ public:
     enum { IDD = IDD_DIALOG_LogIn
     };
 
-    BEGIN_MSG_MAP(LogIn_Dialog)
         MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+        COMMAND_ID_HANDLER(IDOK, OnOK)
         COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
         COMMAND_ID_HANDLER_EX(ID_LogIn, OnBTN_LogIn)
     END_MSG_MAP()
@@ -21,7 +21,6 @@ public:
     void CenterWindow(HWND hWnd = NULL);
     LRESULT OnOK(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
     LRESULT OnCancel(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
-    void OnBTN_LogIn(UINT uNotifyCode, int nID, CWindow wndCtl);
     void InitUi();
 
 public:
