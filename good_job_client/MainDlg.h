89c6cc6767fbce26a41270e2d027535d7013e038
“SctoC” <hanxiangqian2021@163.com>
2024-08-02 23:06:47 +0800

    MainDlg.h与移植到MainDlg.c

--- good_job_client/MainDlg.h
+++ good_job_client/MainDlg.h
@@ -11,16 +11,11 @@ class CMainDlg : public CDialogImpl<CMainDlg>, public CUpdateUI<CMainDlg>,
 public:
 	enum { IDD = IDD_MAINDLG };
 
-	virtual BOOL PreTranslateMessage(MSG* pMsg)
-	{
-		return CWindow::IsDialogMessage(pMsg);
-	}
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
 
-	virtual BOOL OnIdle()
-	{
-		UIUpdateChildWindows();
-		return FALSE;
-	}
+
+	virtual BOOL OnIdle();
+
 
 	BEGIN_UPDATE_UI_MAP(CMainDlg)
 	END_UPDATE_UI_MAP()
@@ -38,76 +33,21 @@ public:
 //	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 //	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
 
-	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
-	{
-		// center the dialog on the screen
-		CenterWindow();
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
 
-		// set icons
-		HICON hIcon = AtlLoadIconImage(IDR_MAINFRAME, LR_DEFAULTCOLOR, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON));
-		SetIcon(hIcon, TRUE);
-		HICON hIconSmall = AtlLoadIconImage(IDR_MAINFRAME, LR_DEFAULTCOLOR, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON));
-		SetIcon(hIconSmall, FALSE);
 
-		// register object for message filtering and idle updates
-		CMessageLoop* pLoop = _Module.GetMessageLoop();
-		ATLASSERT(pLoop != NULL);
-		pLoop->AddMessageFilter(this);
-		pLoop->AddIdleHandler(this);
+	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
 
-		UIAddChildWindowContainer(m_hWnd);
+	
+	LRESULT OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 
-		CString account;
-		CString pwd;
-		LogIn_Dialog dlg(account, pwd);
-		if (dlg.DoModal() == IDOK)
-		{
-			//·¢ËÍÇëÇó¡£
-			int a = 1;
-		}
-		else
-		{
-		  CloseDialog(IDOK);
-		   return 0;
-		}
 
-		return TRUE;
-	}
+	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 
-	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
-	{
-		// unregister message filtering and idle updates
-		CMessageLoop* pLoop = _Module.GetMessageLoop();
-		ATLASSERT(pLoop != NULL);
-		pLoop->RemoveMessageFilter(this);
-		pLoop->RemoveIdleHandler(this);
 
-		return 0;
-	}
+	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 
-	LRESULT OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-	{
-		CAboutDlg dlg;
-		dlg.DoModal();
-		return 0;
-	}
 
-	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-	{
-		// TODO: Add validation code 
-		CloseDialog(wID);
-		return 0;
-	}
+	void CloseDialog(int nVal);
 
-	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-	{
-		CloseDialog(wID);
-		return 0;
-	}
-
-	void CloseDialog(int nVal)
-	{
-		DestroyWindow();
-		::PostQuitMessage(nVal);
-	}
 };
