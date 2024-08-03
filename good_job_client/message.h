883a6b31b064aec8a85a190aaa28ef4f54165453
“SctoC” <hanxiangqian2021@163.com>
2024-08-03 11:42:33 +0800

    修改请求对象的继承方式：以使派生类指针能够转为基类指针。

--- good_job_client/message.h
+++ good_job_client/message.h
@@ -2,13 +2,14 @@
 #include<string>
 
 #define LogInQuest 1
+
 class Request
 {
 public:
 	Request(unsigned int _type):type(_type){}
 	unsigned int type;
 };
-class LogIn_Request :Request
+class LogIn_Request :public Request
 {
 public:
 	LogIn_Request(unsigned int _type, std::string _account, std::string _pwd) :Request(_type), account(_account), pwd{ _pwd }{}
