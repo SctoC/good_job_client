7889c3f956e8fabd96155d42f94d7bd2bcb4cc19
“SctoC” <hanxiangqian2021@163.com>
2024-08-09 14:06:48 +0800

    接收应答信息线程

---
+++ good_job_client/ReceiveAckThread.cpp
@@ -0,0 +1,87 @@
+#pragma once
+#include "ReceiveAckThread.h"
+
+
+ReceiveAckThread::ReceiveAckThread() :_stop(false)
+{
+	start();
+}
+ReceiveAckThread::~ReceiveAckThread()
+{
+	stop();
+	receAckThread.join();
+}
+void ReceiveAckThread::start()
+{
+	receAckThread = std::thread(&ReceiveAckThread::receive_threadfuntion, this);
+}
+void ReceiveAckThread::stop()
+{
+	_stop = true;
+}
+void ReceiveAckThread::AddAck(std::string ackData)
+{
+	mtx.lock();
+	receiveQue.push(ackData);
+	mtx.unlock();
+}
+void ReceiveAckThread::receive_threadfuntion()
+{
+
+	//存在死锁问题
+	while (!_stop)
+	{
+		
+		bool is_unlock;
+		mtx.lock();
+		is_unlock = true;
+		//此处可以优化为条件变量
+		if (!receiveQue.empty())
+		{
+			std::string jsonData=receiveQue.front();
+			receiveQue.pop();
+			mtx.unlock();
+			is_unlock = false;
+			HandleAck(jsonData);
+		}
+		if (is_unlock)
+		{
+			mtx.unlock();
+		}
+	}
+}
+void ReceiveAckThread::HandleAck(std::string& jsonData )
+{
+	// 解析 JSON 数据
+	Json::Value root;
+	Json::CharReaderBuilder reader;
+	std::string errs;
+
+	int type = 0;
+	std::istringstream ss(jsonData);
+	if (Json::parseFromStream(reader, ss, &root, &errs)) {
+		// 访问解析后的 JSON 数据
+		type = root["type"].asInt();
+	}
+	//这里可以考虑用多态
+	switch (type)
+	{
+	case 1:
+		HandleLogInAck(root);
+		break;
+	default:
+		break;
+	}
+}
+void ReceiveAckThread::HandleLogInAck(Json::Value& root)
+{
+	bool isSuccess = root["isSuccess"].asBool();
+	if (isSuccess)
+	{
+
+	}
+	else
+	{
+
+	}
+}
---
+++ good_job_client/ReceiveAckThread.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include<queue>
+#include<atomic>
+#include<mutex>
+#include<thread>
+#include<string>
+#include "message.h"
+#include "json.h" // 包含 JsonCpp 库的头文件
+
+class ReceiveAckThread
+{
+
+public:
+	ReceiveAckThread();
+	~ReceiveAckThread();
+
+	void start();
+
+	void stop();
+
+	void AddAck(std::string ackData);
+
+	void receive_threadfuntion();
+
+	void HandleAck(std::string& jsonData);
+
+	void HandleLogInAck(Json::Value& root);
+
+	std::thread receAckThread;
+
+	//可以优化成智能指针
+	std::queue<std::string> receiveQue;
+	std::atomic<bool> _stop;
+	std::mutex mtx;
+};
