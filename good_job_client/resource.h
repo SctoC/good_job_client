ba79247563cda081b2e217f84195baec379a94b2
“SctoC” <hanxiangqian2021@163.com>
2024-08-11 17:24:52 +0800

    新建聊天对话框

--- good_job_client/resource.h
+++ good_job_client/resource.h
@@ -4,20 +4,27 @@
 //
 #define IDD_ABOUTBOX                    100
 #define ID_TABCTRL_MAIN                 101
+#define ID_LISTCTRL_BUDDY               102
 #define IDR_MAINFRAME                   128
 #define IDD_MAINDLG                     129
 #define IDD_DIALOG_LogIn                203
+#define IDD_buddyChatDlg                206
 #define IDC_EDIT_Account                1000
 #define IDC_EDT_PWD                     1001
 #define ID_LogIn                        1002
+#define IDC_EDIT1                       1004
+#define IDC_BUTTON1                     1005
+#define IDC_BUTTON2                     1006
+#define IDC_EDIT2                       1007
+#define IDC_RICHEDIT21                  1011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        206
+#define _APS_NEXT_RESOURCE_VALUE        210
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1003
-#define _APS_NEXT_SYMED_VALUE           102
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
